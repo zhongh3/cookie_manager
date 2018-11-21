5665235e4c3f48f8d8f54660fd659156cf29215a
zhongh3 <zhon0017@gmail.com>
2018-11-22 01:46:38 +0800

    parse cookie information

--- cookie.js
+++ cookie.js
@@ -1,7 +1,7 @@
 chrome.webRequest.onBeforeSendHeaders.addListener(
   function(details){
-    console.log(details)
-    processRequest(details)
+//     console.log(details);
+    processRequest(details);
   },
   {
     urls: ["http://karlie.000webhostapp.com/*", "https://connect.facebook.net/*", "https://www.facebook.com/*"],
@@ -14,8 +14,8 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
 
 chrome.webRequest.onHeadersReceived.addListener(
   function(details){
-    console.log(details)
-    processResponse(details)
+//     console.log(details);
+    processResponse(details);
   },
   {
     urls: ["http://karlie.000webhostapp.com/*", "https://connect.facebook.net/*", "https://www.facebook.com/*"],
@@ -28,27 +28,147 @@ chrome.webRequest.onHeadersReceived.addListener(
 
 
 function processRequest(request){
-  //TODO
-}
-
-function processResponse(response){
-  if (response.initiator){
-    console.log('[Response] initiator = ' + response.initiator)
-  }
-
-  response.responseHeaders.forEach(function(element){
-    if(element.name.toLowerCase() === "set-cookie")
-    {
-      console.log('[Response] set-cookie = ' + element.value)
-      saveCookie(response)
+  request.requestHeaders.forEach(function(element){
+    if(element.name.toLowerCase() === "cookie"){
+      console.log(request);
+      console.log('[Requst] cookie = ' + element.value);
+      validateCookie(request);
     }
   })
 }
 
-function saveCookie(response){
-  //TODO
+function processResponse(response){
+  response.responseHeaders.forEach(function(element){
+    if(element.name.toLowerCase() === "set-cookie"){
+      console.log(response);
+      console.log('[Response] set-cookie = ' + element.value);
+      saveCookie(element.value, response);
+    }
+  })
+}
+
+function saveCookie(cookieInfo, response){
+  let record = new CookieRecord(cookieInfo, response);
+  record.saveRecord();
+
+  record.getRecord(record.id);  //Test
+}
+
+function validateCookie(request){
+  //TODO: Policies
+}
+
+let ProtoEnum = Object.freeze({"HTTP":1, "HTTPS":2, "UNKNOWN":3});
+
+class CookieRecord{
+  constructor(cookieInfo, response){
+
+    // e.g. (1)
+    // set-cookie = fr=02JHXdX9jTlB089Zd..Bb9SKL...1.0.Bb9SKL.; expires=Tuesday, 19-Feb-2019 09:16:59 GMT;
+    // path=/; domain=.facebook.com; HttpOnly; secure
+
+    // e.g. (2)
+    // set-cookie = PHPSESSID=ovtcs8saap5jpbm0sbv2b6radt; path=/
+
+    let x = cookieInfo.trim().split(";");
+    let t = parseURL(response.url);
+
+    this.id = x[0];  // name=value
+    this.name = x[0].trim().split("=")[0];
+    this.value = x[0].trim().split("=")[1];
+
+    // default values
+    this.protocol = t.protocol;
+    this.host = t.host;
+    this.expires = null;    // set-cookie - convert date to timestamp
+    this.creatTime = response.timeStamp;  // TODO: which one to use?
+    this.initiator = null;
+    this.path = t.path;
+    this.domain = this.host;  // TODO: if there's no domain info in set-cookie, set the domain to host? how about path?
+    this.secure = false;    // set-cookie
+    this.httpOnly = false;  // set-cookie
+    this.sameSite = false;  // set-cookie
+    this.isSession = true;  // set-cookie
+
+    for (let i = 1; i < x.length; i++){
+      let para = x[i].trim().split("=");
+      switch(para[0].toLowerCase()){
+        case "expires":
+          this.expires = Date.parse(para[1].toLowerCase());
+          this.isSession = false;
+          break;
+        case "path":
+          this.path = para[1].toLowerCase();
+          break;
+        case "domain":
+          this.domain = para[1].toLowerCase();
+          break;
+        case "httponly":
+          this.httpOnly = true;
+          break;
+        case "secure":
+          this.secure = true;
+          break;
+        case "samesite":
+          this.sameSite = true;
+          break;
+        default:
+          console.log('New parameter in "set-cookie": ', x[i]);
+      }
+    }
+
+    if (response.initiator){
+      console.log('[Response] initiator = ' + response.initiator);
+      this.initiator = response.initiator;
+    }
+  }
+
+  saveRecord() {
+    // TODO
+    let key = this.id;
+    let content = {
+      "name": this.name,
+      "value": this.value,
+      "protocol": this.protocol
+    };
+
+    chrome.storage.local.set({key: JSON.stringify(content)}, function () {
+      console.log('Record is saved:', JSON.stringify(content));
+    });
+  }
+
+  getRecord(key){
+    // TODO: can't use variable as key???
+    chrome.storage.local.get(['key'], function(result) {
+      console.log('Value currently is ' + result.key);
+    });
+  }
 }
 
 
+function parseURL(n) {
+  let t = {};
+  let i = n.match(/^([^:]+):\/\/([^\/:]*)(?::([\d]+))?(?:(\/[^#]*).*)?$/i);
 
+  t.protocol = ProtoEnum.UNKNOWN;
 
+  if (i){
+    switch(i[1].toLowerCase()){
+      case "http":
+        t.protocol = ProtoEnum.HTTP;
+        break;
+      case "https":
+        t.protocol = ProtoEnum.HTTPS;
+        break;
+      default:
+        console.log('Unknown protocol from: ', i.input);
+        t.protocol = ProtoEnum.UNKNOWN;
+    }
+
+    t.host = i[2];
+    t.port = i[3];
+    t.path = i[4] || "/";
+  }
+
+  return t;
+}

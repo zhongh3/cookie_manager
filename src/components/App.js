6192d847d119d57bf85700a38ad535c79cd6475e
Shaohua Huang <hshnju@gmail.com>
2018-11-24 15:14:41 +0800

    save

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import RequestList from './RequestList'
 
 const App = () => (
-	<div>
-		sample
+	<div style={{display: "flex"}}>
+		<div style={{"width": "30%"}}>
+			<RequestList/>
+		</div>
+		<div style={{width: "70%", border: "1px solid rgba(0, 0, 0, 0.12)"}}>
+			test
+		</div>
 	</div>
 )
 
---
+++ src/components/RequestList.js
@@ -0,0 +1,15 @@
+import {List, ListItem, ListItemText} from '@material-ui/core';
+import React from "react"
+
+const RequestList = () => (
+	<List>
+		<ListItem divider>
+			<ListItemText>Test</ListItemText>
+		</ListItem>
+		<ListItem divider>
+			<ListItemText>Test1</ListItemText>
+		</ListItem>
+	</List>
+)
+
+export default RequestList

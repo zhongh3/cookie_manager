b18af430a3b47e89b445d1fa7f65b8cae43f14ee
Shaohua Huang <hshnju@gmail.com>
2018-11-24 11:12:33 +0800

    refactor

--- cookie.js
+++ cookie.js
@@ -138,31 +138,3 @@ class CookieRecord{
     return JSON.parse(localStorage.getItem(key));
   }
 }
-
-
-function parseURL(n) {
-  let t = {};
-  let i = n.match(/^([^:]+):\/\/([^\/:]*)(?::([\d]+))?(?:(\/[^#]*).*)?$/i);
-
-  t.protocol = ProtoEnum.UNKNOWN;
-
-  if (i){
-    switch(i[1].toLowerCase()){
-      case "http":
-        t.protocol = ProtoEnum.HTTP;
-        break;
-      case "https":
-        t.protocol = ProtoEnum.HTTPS;
-        break;
-      default:
-        console.log('Unknown protocol from: ', i.input);
-        t.protocol = ProtoEnum.UNKNOWN;
-    }
-
-    t.host = i[2];
-    t.port = i[3];
-    t.path = i[4] || "/";
-  }
-
-  return t;
-}
---
+++ util.js
@@ -0,0 +1,26 @@
+function parseURL(n) {
+	let t = {};
+	let i = n.match(/^([^:]+):\/\/([^\/:]*)(?::([\d]+))?(?:(\/[^#]*).*)?$/i);
+	
+	t.protocol = ProtoEnum.UNKNOWN;
+	
+	if (i){
+		switch(i[1].toLowerCase()){
+			case "http":
+				t.protocol = ProtoEnum.HTTP;
+				break;
+			case "https":
+				t.protocol = ProtoEnum.HTTPS;
+				break;
+			default:
+				console.log('Unknown protocol from: ', i.input);
+				t.protocol = ProtoEnum.UNKNOWN;
+		}
+		
+		t.host = i[2];
+		t.port = i[3];
+		t.path = i[4] || "/";
+	}
+	
+	return t;
+}
